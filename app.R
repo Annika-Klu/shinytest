b533ab2b7a961d8dee17e059353deeca9b4d50b6
Annika <68771358+Annika-Klu@users.noreply.github.com>
2024-05-08 13:54:51 +0200

    test explicit stdout msg

--- app.R
+++ app.R
@@ -1,5 +1,8 @@
 library(shiny)
 
+# let's see if this gets logged
+cat("This is a test message\n", file=stdout())
+
 ui <- fluidPage(
   titlePanel("Hello"),
   textInput("email", "Your Email", ""),
